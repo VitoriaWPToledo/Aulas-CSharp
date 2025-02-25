8f097ef9507ca945821cc3e4e92741e24b76611d
vitoria.wptoledo <vitoria.wptoledo@TIT0545526W10-1>
2025-02-25 19:27:18 -0300

    Adicionar arquivos de projeto.

---
+++ Aula1/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Hello, World!");
+Console.Write("viih\n");
+Console.Write("zn\n");
+
+
+string firtname = "Vitória";
+string lastname = "de Toledo";
+string surname = "Watanabe Primante";
+int idade = 21;
+
+Console.WriteLine($"Meu nome completo é:{firtname} {surname} {lastname} e a minha idade é:{idade}");
+
+Console.WriteLine("Escreva o seu nome: ");
+string nome = Console.ReadLine();
+Console.WriteLine($" Olá {nome}, boa noite!!!");
+
